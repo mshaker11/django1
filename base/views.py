68707988818221ba98550ca3d9ffb6c0da5232b1
Mohamed Nour <mohamedshaker@Mohameds-MacBook-Air-3.local>
2025-04-27 19:59:14 -0400

    Completed Flash Messages (2:32:25)

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import Item
 
@@ -9,22 +10,26 @@ def createItem(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         Item.objects.create(name=name)
+        messages.success(request, "Item created successfully!")
         return redirect('/')
     return render(request, 'create_item.html')
+
 def updateItem(request, pk):
     item = Item.objects.get(id=pk)
 
     if request.method == 'POST':
         item.name = request.POST.get('name')
         item.save()
+        messages.success(request, "Item updated successfully!")
         return redirect('/')
-
     return render(request, 'update_item.html', {'item': item})
+
 def deleteItem(request, pk):
     item = Item.objects.get(id=pk)
 
     if request.method == 'POST':
         item.delete()
+        messages.success(request, "Item deleted successfully!")
         return redirect('/')
-
     return render(request, 'delete_item.html', {'item': item})
+
