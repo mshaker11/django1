af009e814d8558cd9f7c04791f0ce50d8c5d2c26
Mohamed Nour <mohamedshaker@Mohameds-MacBook-Air-3.local>
2025-04-27 19:37:45 -0400

    Full CRUD app completed

--- base/views.py
+++ base/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
 
 def home(request):
-    return render(request, 'home.html')
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
+
+def createItem(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        Item.objects.create(name=name)
+        return redirect('/')
+    return render(request, 'create_item.html')
+def updateItem(request, pk):
+    item = Item.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.name = request.POST.get('name')
+        item.save()
+        return redirect('/')
+
+    return render(request, 'update_item.html', {'item': item})
+def deleteItem(request, pk):
+    item = Item.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    return render(request, 'delete_item.html', {'item': item})
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path  # <-- THIS is what you were missing
 from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('create/', views.createItem, name='create'),
+    path('update/<str:pk>/', views.updateItem, name='update'),
+    path('delete/<str:pk>/', views.deleteItem, name='delete'),
 ]
